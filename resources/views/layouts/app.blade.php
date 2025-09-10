7645d09971a7faaa850a99ba4f36bb4dd2f43585
hsn771 <hr492785@gmail.com>
2025-09-10 12:42:31 +0600

    sunni shibir

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -584,8 +584,8 @@
                   <script>
                     document.write(new Date().getFullYear());
                   </script>
-                  , made with ❤️ by
-                  <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+                  , made by
+                  <a href="#" target="_blank" class="footer-link fw-bolder">Tanvir Islam(Sunni Shibir)</a>
                 </div>
                 <div>
                   <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
